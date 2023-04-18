407d7994bc4a76db2cae7e407c053350375ba46f
fshangala <fshangala@gmail.com>
2023-04-18 10:40:04 +0200

    business public view

--- businesses/urls.py
+++ businesses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
-  BusinessList
+  BusinessList,
+  BusinessView
 )
 
 app_name="businesses"
 urlpatterns = [
   path('',BusinessList.as_view(),name="list"),
+  path('<pk>/',BusinessView.as_view(),name="detail"),
 ]
--- businesses/views.py
+++ businesses/views.py
@@ -4,14 +4,28 @@ from .models import Business
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class BusinessView(LoginRequiredMixin,View):
-  template_name="businesses/business_detail.html"
-  def get(self,request):
-    context=self.get_context_data()
+class BuyStock(LoginRequiredMixin,View):
+  template_name="businesses/buy_stock.html"
+  def get(self,request,pk):
+    context=self.get_context_data(pk)
     return render(request, self.template_name, context=context)
   
-  def get_context_data(self):
-    context = {}
+  def get_context_data(self,pk):
+    context = {
+      "business":Business.objects.get(pk=pk)
+    }
+    return context
+  
+class BusinessView(LoginRequiredMixin,View):
+  template_name="businesses/business_detail.html"
+  def get(self,request,pk):
+    context=self.get_context_data(pk)
+    return render(request, self.template_name, context=context)
+  
+  def get_context_data(self,pk):
+    context = {
+      "business":Business.objects.get(pk=pk)
+    }
     return context
   
 class BusinessList(generic.ListView):
